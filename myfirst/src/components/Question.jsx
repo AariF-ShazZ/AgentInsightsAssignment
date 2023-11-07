431b72fca5ab06318b778d1bc69f20bf49f35609
Aarif-shazZ <shazaarif0@gmail.com>
2023-11-07 11:50:27 +0530

    done

--- myfirst/src/components/Question.jsx
+++ myfirst/src/components/Question.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Box, Button, VStack } from '@chakra-ui/react';
-
-const Question = ({ question, options, handleAnswer }) => {
+import { Box, Button, Flex, VStack } from '@chakra-ui/react';
+const Question = ({ question, options, handleAnswerSubmit }) => {
     return (
         <VStack spacing={4} align="stretch" maxW="400px" mx="auto">
-            <div className="card">
-            <Box p={4} borderWidth="1px" borderRadius="md">
-                <h2>{question}</h2>
+          <Flex
+            className="card"
+            w={{ base: "200px", sm: "200px", md: "500px", lg: "500px" }}
+            // border={"2px solid red"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDirection={{ base: "column", sm: "column", md: "row", lg: "row" }}
+            p={"6%"}
+          >
+            <Box p={4} borderWidth="1px" borderRadius="md" color={"red"} mr={2} >
+              <h2>{question}</h2>
             </Box>
-                <VStack  spacing={4} align="stretch" maxW="400px" mx="auto">
-                {options.map(option => (
-                <Button key={option} onClick={() => handleAnswer(option)} variant="outline">
-                    {option}
+            <VStack spacing={4} align="stretch" maxW="400px" mx="auto">
+              {options.map(option => (
+                <Button
+                  className="button-62"
+                  role="button"
+                  key={option}
+                  onClick={() => handleAnswerSubmit(option)}
+                  variant="outline"
+                >
+                  {option}
                 </Button>
-                // <button key={option} onClick={() => handleAnswer(option)}>{option}</button>
-            ))}
-                </VStack>
-          
-            </div>
-           
+              ))}
+            </VStack>
+          </Flex>
         </VStack>
-    );
+      );
 };
-
 export default Question;
--- myfirst/src/components/Quiz.jsx
+++ myfirst/src/components/Quiz.jsx
@@ -2,45 +2,50 @@ import React, { useState } from 'react';
 import Question from './Question';
 import Result from './Result';
 import questions from '../questions.json';
-// import './Quiz.css';
 
 const Quiz = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
-  console.log(questions);
-  const handleAnswer = (selectedAnswer) => {
-    setUserAnswers([...userAnswers, selectedAnswer]);
-    setCurrentQuestion(currentQuestion + 1);
-  };
-
-  const calculateScore = () => {
-    let score = 0;
-    userAnswers.forEach((answer, index) => {
-      if (answer === questions.questions[index].answer) {
-        score++;
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+  
+    const handleAnswerSubmit = (selectedAnswer) => {
+      const isCorrect = selectedAnswer === questions.questions[currentQuestion].answer;
+      setUserAnswers([...userAnswers, { question: questions.questions[currentQuestion].question, answer: selectedAnswer, isCorrect }]);
+      if (isCorrect) {
+        setScore(score + 1);
       }
-    });
-    return score;
-  };
-
-  if (currentQuestion < questions.questions.length) {
-    return (
-      <div className="quiz-container">
+      if (currentQuestion < questions.questions.length - 1) {
+        setCurrentQuestion(currentQuestion + 1);
+      }
+    };
+  
+    const handlePrevQuestion = () => {
+      if (currentQuestion > 0) {
+        setCurrentQuestion(currentQuestion - 1);
+      }
+    };
+  
+    const handleNextQuestion = () => {
+      if (currentQuestion < questions.questions.length - 1) {
+        setCurrentQuestion(currentQuestion + 1);
+      }
+    };
+  return (
+    <div className="quiz-container">
+      {currentQuestion < questions.questions.length ? (
         <Question
           question={questions.questions[currentQuestion].question}
           options={questions.questions[currentQuestion].options}
-          handleAnswer={handleAnswer}
+          handleAnswerSubmit={handleAnswerSubmit}
         />
-      </div>
-    );
-  } else {
-    const score = calculateScore();
-    return (
-      <div className="quiz-container">
+      ) : (
         <Result score={score} totalQuestions={questions.questions.length} />
+      )}
+      <div className="nav-buttons">
+        <button onClick={handlePrevQuestion} disabled={currentQuestion === 0}>Previous</button>
+        {currentQuestion < questions.questions.length - 1 && <button onClick={handleNextQuestion}>Next</button>}
       </div>
-    );
-  }
+    </div>
+  );
 };
-
 export default Quiz;
